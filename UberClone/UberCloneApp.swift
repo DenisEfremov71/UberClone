aeabda33ed5215ed1c5d68f882c40ead1f8916ad
Denis Efremov <efremov@novuscom.net>
2023-11-07 04:39:13 -0800

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Denis Efremov on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
